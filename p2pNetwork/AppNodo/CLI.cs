734bef1d8cad1b85f88dd4ba3572d469bb94efbe
pviroulaud <pablo_viroulaud@yahoo.com.ar>
2022-03-14 10:14:53 -0300

    clase para la gestion de los comandos

---
+++ p2pNetwork/AppNodo/CLI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNodo
+{
+    public class CLI
+    {
+        public CLI()
+        {
+
+        }
+        public void printMenu()
+        {
+            Console.WriteLine("Comandos:");
+            Console.WriteLine();
+
+            Console.WriteLine("  help\t\tImprime esta ayuda.");
+            Console.WriteLine("  lclientes\t\tImprime los clientes conectados a este nodo.");
+            Console.WriteLine("  getPescucha\t\tImprime el puerto de escucha para conexiones entrantes.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+        public void printAbout()
+        {
+            Console.WriteLine("************* Nodo de red P2P **************");
+            Console.WriteLine("Desarrollado por Pablo Daniel Viroulaud 2022.");
+        }
+    }
+}
